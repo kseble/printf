96c13d8a287e8967c1d2658228d21abc2f5843e3
kseble <seble.temesgenk@gmail.com>
2023-09-08 07:45:10 -0700

    add

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - prints given string
+ *
+ * Description: a program to print string
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	_printf("characters printed\n");
+
+	return (_printf)
+}
